925e9d40d0fbb4cf384a02673e443d6a882ca3c9
LZhang888 <zhanglinghe610@gmail.com>
2023-08-02 02:27:20 -0700

    BUG FIX FOR UNMATCHING SCREEN NAMES

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 import json
+from flask import Flask, redirect, url_for, session, request
 from datetime import datetime
 
 class Environment:
@@ -11,20 +12,19 @@ class Environment:
 
         os.environ["SPOTIPY_CLIENT_ID"] = "ed5cbf589898495db2525f4a918ecdd5"
         os.environ["SPOTIPY_CLIENT_SECRET"] = "cf8d56c29a9c45b983e7933ebbee009b"
-        os.environ["SPOTIPY_REDIRECT_URI"] = "http://127.0.0.1:8080"
+        os.environ["SPOTIPY_REDIRECT_URI"] = "http://127.0.0.1:8080/callback"
 
         #Client Login 
         client_credentials_manager = SpotifyClientCredentials(client_id='ed5cbf589898495db2525f4a918ecdd5', client_secret='cf8d56c29a9c45b983e7933ebbee009b')
-        self.sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+        self.sp = sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
         #Authenticate With Account and specify permissions
         scope = 'playlist-modify-public playlist-modify-private'
 
         #Change to yours if you want to do testing on your computer
-        self.username = username = '9upv4ku9el0ve5vp4m56rvbm8'
-
+        self.username = username = session["spotify_username"]
+        print(str(username))
         #Object and Token Creation
-
         token = SpotifyOAuth(scope=scope, username=username)
         self.spotifyObject = spotipy.Spotify(auth_manager = token)
 
@@ -47,4 +47,6 @@ class UserInput:
         link = input("Paste the link of the playlist you want to copy from: ")
         self.playlist_copy_from = Tools.cleanUpLink(link)
         self.reverseYear = Tools.stringCondition(input("New to Old (ENTER: True), Old to New (ENTER: False): ").lower())
+
+
 
